a8ca067a507e0fba034f402ac531bd2af58bb23a
ChristmasLover12345 <madnes0205@gmail.com>
2025-01-28 16:02:40 -0800

    Started on backend

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,18 @@ let pmElement = document.getElementById("pmElement")
 let pmLocation = document.getElementById("pmLocation")
 let pmMoves = document.getElementById("pmMoves")
 let pmAbilities = document.getElementById("pmAbilities")
+
 let addFavBtn = document.getElementById("addFavBtn")
+let shinyBtn = document.getElementById("shinyBtn")
 let favBtn = document.getElementById("favBtn")
 let randomBtn = document.getElementById("randomBtn")
 let searchBar = document.getElementById("searchBar")
+
+let isShiny = false;
+let shinyUrl = ""
+let normalUrl = ""
+
+let 
 
 // Fetches
 async function pokemonFetch(userSearch)
@@ -21,10 +29,51 @@ async function pokemonFetch(userSearch)
 
 
 
+// Logic
+async function getInfo(userMon)
+{
+    let pokeInfo = await pokemonFetch(userMon)
+
+    if (pokeInfo.id > 649)
+    {
+    alert("Only gen 1 - 5")
+    return
+    }
+
+    normalUrl = pokeInfo.sprites.front_default
+    shinyUrl = pokeInfo.sprites.front_shiny
+
+    pmImg.src = pokeInfo.sprites.front_default
+    pmName.innerText = pokeInfo.name
+
+    let locations = await (fetch(pokeInfo.location_area_encounters)).json()
+
+    pmLocation.innerText = locations
+
+
+}
+
+
 searchBar.addEventListener("keydown", (event) => {
     if (event.key ==="Enter")
-    {
-        pokemonFetch(searchBar.value)
+    {   
+        isShiny = false
+        getInfo(searchBar.value)
     }
+})
+
+shinyBtn.addEventListener('click', () => {
+    
+    if (!isShiny)
+    {
+        isShiny = true
+        pmImg.src = shinyUrl
+    }
+    else if (isShiny)
+    {
+        isShiny = false
+        pmImg.src = normalUrl
+    }
+
 })
 
